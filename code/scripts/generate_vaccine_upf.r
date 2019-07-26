d4a3a3efc80a42a83b7d4f8b8d897e3f9d9224c0
Eric Tatara <tatara@anl.gov>
2019-07-26 10:38:37 -0500

    Script and UPFs for generating vaccine sweep runs

--- code/scripts/generate_vaccine_upf.r
+++ code/scripts/generate_vaccine_upf.r
@@ -5,10 +5,18 @@
 # Eric Tatara
 #
 
-vaccine_schedule <- c('D1', 'D2a', 'D2b', 'D3a', 'D3b')
-vaccine_study_arm_n <- c(797, 1599, 797, 797, 797)
+#vaccine_schedule <- c('D1', 'D2a', 'D2b', 'D3a', 'D3b')
+#vaccine_study_arm_n <- c(797, 1599, 797, 797, 797)
 
-replicates <- 100          # Number of replicates per rate param value 
+#vaccine_schedule <- c('D1', 'D2b', 'D3b', 'D1', 'D2b', 'D1', 'D2b', 'D3b', 'D1', 'D2b', 'D3b')
+#vaccine_study_arm_n <- c(797, 797, 797, 797, 797, 1594, 1594, 1594, 797, 797, 797)
+#vaccine_followup1_periods <- c(18, 18, 18, 24, 22, 18, 18, 18, 30, 30, 30)
+
+vaccine_schedule <- c('D2a', 'D1', 'D2b', 'D3b')
+vaccine_study_arm_n <- c(797, 1594, 1594, 1594)
+vaccine_followup1_periods <- c(22, 24, 22, 18)
+
+replicates <- 500          # Number of replicates per rate param value 
 
 x <- ""
 i <- 0
@@ -28,7 +36,9 @@ for (vs in vaccine_schedule){
   
     x <- paste0(x,"vaccine_schedule\t",vs,",")
   
-    x <- paste0(x,"vaccine_study_arm_n\t",vaccine_study_arm_n[arm])
+    x <- paste0(x,"vaccine_study_arm_n\t",vaccine_study_arm_n[arm],",")
+    
+    x <- paste0(x,"vaccine_followup1_periods\t",vaccine_followup1_periods[arm])
     
     x <- paste0(x,"\n")
   
@@ -36,4 +46,4 @@ for (vs in vaccine_schedule){
   arm <- arm + 1
 }
 
-write(x, file="upf_vaccine_sweep.txt")
+write(x, file="upf_vaccine_sweep_3.txt")
